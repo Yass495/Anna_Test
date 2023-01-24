def1a517bcd279160222e24fde7a79aa03d3cdb0
Yass495 <barchat.mohamed.sio@gmail.com>
2023-01-24 16:17:08 +0100

    mise a joure du javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+// Original Code By Webdevtrick ( https://webdevtrick.com )
+function showTime(){
+	var date = new Date();
+	var h =date.getHours();
+	var m= date.getMinutes();
+	var s =date.getSecondes():
+
+	h = (h < 10) ? "0" + h : h;
+	m = (m < 10) ? "0" + m : m;
+	s = (s  < 10) ? "0" + s : s;
+
+	var time = h + ":" + m + ":" + s + " " + session;
+	document.getElementById("DigitaleCLOCK").innerTEXT = time;
+	document.getElementById("DigitaleCLOCK").innerContent = time;
+
+	setTimeout(showTime, 1000);
+
+}
+showTime();
